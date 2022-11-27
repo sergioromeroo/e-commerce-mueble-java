153249895a20ce999de0e7dbf1cd029bed9f926d
ErickLopez444-a <erickbale11rm@hotmail.com>
2022-11-27 13:58:43 -0700

    "Agregar Sweet alerts en el login y register"

--- src/main/resources/static/web/js/login-register.js
+++ src/main/resources/static/web/js/login-register.js
@@ -24,20 +24,47 @@ createApp({
     methods: {
 
         access() {
+       
             axios.post('/api/login', `email=${this.emailVModel}&password=${this.passwordVModel}`)
             .then(response => {
                 console.log(response)
                 window.location.reload()
             })
-            .catch(error => console.log(error))
+            .catch(function (error){
+                Swal.fire({
+                    title: 'Wrong Password or UserMail Check Again',
+                    confirmButtonColor: 'lightgray',
+                    confirmButtonText:'Try again',
+                    timer: 2500
+                  
+                  })
+            })
         },
         clientRegister() {
+            if (!this.emailVModel.includes("@")) {
+                Swal.fire({
+                    text: `The email Must Contain @ to be valid`,
+                    confirmButtonColor: 'lightgrey',
+                    timer: 5500
+                })
+            }
+            
             axios.post('/api/clients', `firstName=${this.firstNameRegisterVModel}&lastName=${this.lastNameRegisterVModel}&email=${this.emailVModel}&password=${this.passwordVModel}&cellphone=${this.celPhoneRegisterVModel}`)
             .then(response => {
                 console.log(response)
                 this.access()
             })
-            .catch(error => console.log(error))
+            
+            .catch( function(error) {
+                Swal.fire({
+                    icon: 'error',
+                    title: error.response.data,
+                    confirmButtonColor: 'lightgray',
+                    confirmButtonText:'Ok',
+
+                })
+            })
+            
         },
 
         theLoginAndRegister(){
