0dd409e4447b0fde5e7349b35144999828620663
Maria Dominguez <ro.antarez@gmail.com>
2022-11-27 21:10:49 -0300

    productos en TicketProductDTO

--- src/main/java/com/mindhub/ecommerce/DTOS/TicketProductDTO.java
+++ src/main/java/com/mindhub/ecommerce/DTOS/TicketProductDTO.java
@@ -11,7 +11,11 @@ public class TicketProductDTO {
 
     private long idProduct;
 
+    private String name;
 
+    private String type;
+
+    private double price;
 
 
     public TicketProductDTO() {
@@ -19,15 +23,28 @@ public class TicketProductDTO {
 
     public TicketProductDTO(TicketProduct ticketProduct) {
 
-
         this.idProduct = ticketProduct.getProduct().getId();
+        this.name = ticketProduct.getProduct().getName();
+        this.type = ticketProduct.getProduct().getType();
+        this.price = ticketProduct.getProduct().getPrice();
 
     }
-
-
 
 
     public long getIdProduct() {
         return idProduct;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
 }
